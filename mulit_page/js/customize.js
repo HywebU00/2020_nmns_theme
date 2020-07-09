4a852dc24c986740a1872338baf84318ab8b19c4
anniewu <annie@hyweb.com.tw>
2020-07-09 16:25:31 +0800

    20200709我要報名頁面修改上傳

--- mulit_page/js/customize.js
+++ mulit_page/js/customize.js
@@ -149,7 +149,33 @@ $(function() {
         }
     });
 });
+/*-----------------------------------*/
+/////////////燈箱設定/////////////
+/*-----------------------------------*/
+$(function() {
+    // 1
+    $('#modal1').hide(); //先隱藏視窗
+    $('.modal').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal').prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
+    $('.modal_overlay').hide(); //隱藏透明底
+    //按鈕動作
+    $('#openModal').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal() {
+        $('#modal1').fadeOut(400);
+        $('.modal_overlay').fadeOut(400);
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal .close').click(closeModal);
 
+});
 // 我要報名
 // $(function(){
 //      $('.signupbtn').click(function() {
